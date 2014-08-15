a4376a9c3788f7dcda555e989e42afcb46675ee3
arnaud-elasticbox <arnaud@elasticbox.com>
2014-08-15 08:09:12 -0700

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -38,11 +38,16 @@ class rabbitmq::package(
     }
   }
   elsif ($osfamily == 'Debian') {
+    package { $rabbitmq::params::erlang_packages:
+      ensure  => present,
+    }
+    
     exec { 'wget-deb':
       command   => "wget http://www.rabbitmq.com/releases/rabbitmq-server/v${version}/rabbitmq-server_${version}-1_all.deb",
       path      => '/usr/bin:/usr/sbin:/bin:/sbin',
       logoutput => true,
       unless    => "test ! -z $(which rabbitmq-server)",
+      require   => Package[$rabbitmq::params::erlang_packages]
     }
     
     exec { 'install-deb':
